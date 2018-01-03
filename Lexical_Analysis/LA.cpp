3c32c3ecc93f8fa1ef8d59c041ac32a42f2f7f50
TLMichael <756535645@qq.com>
2018-01-03 18:54:17 +0800

    更新了词法分析器

--- Lexical_Analysis/LA.cpp
+++ Lexical_Analysis/LA.cpp
@@ -1,285 +1,261 @@
-/**
- * @lalalal
- * @author TripleZ
- * @date 2017-09-19
- * @brief A simple lexical analysis program written by c++.
- */
-
 #include "LA.h"
 using namespace std;
 
-fstream source;
-fstream output;
-
-const string key[15] = {"program", "const", "var", "procedure", "begin", "if", "end", "while", "call", "read", "write", "then", "odd", "do"};
-
-int line;
-int column;
-
-/**
- * Verify whether is blank char, 1 is BC, 0 is not.
- * @param ch char
- * @author TripleZ
- */
-int isBC(char ch){
-	if (ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n'){
-        switch (ch) {
-            case ' ':
-                column++;
-                break;
-            case '\t':
-                column+=4; // A tab = 4 spaces
-                break;
-            case '\r':
-            case '\n':
-                line++;
-                column = 1;
-                break;
-        }
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-/**
- * Add a character to strToken.
- * @param a strToken
- * @param ch add charater
- * @return new strToken string
- */
-string Concat(string a, char ch) {
-	a.push_back(ch);
-	return a;
-}
-
-/**
- * Check a character whether is a digit.
- * @param ch A character
- * @return 1 is true, 0 is false.
- */
-int isDigit(char ch) {
-	if (ch >= '0' && ch <= '9'){
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-/**
- * Check a character whether is a letter.
- * @param ch A character
- * @return 1 is true, 0 is false.
- */
-int isLetter(char ch){
-	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-/**
- * Check strToken whether is a reserved word.
- * @param strToken search-forward string.
- * @return 1 is reserved, 0 is not.
- */
-int Reserve(string strToken) {
-	int i;
-	for(i = 0; i< 15; i++) {
-		if (strToken.compare(key[i]) == 0) {
-			return 1;// is reserved;
-		}
-	}
-	return 0;
-}
-
-
-/**
- * Move the cursor back a character.
- */
-void Retract(){
-    if (!source.eof()){
-        source.seekg(-1, ios::cur);
+bool isBlank(char ch)
+{
+    switch (ch)
+    {
+        case ' ':
+            column++;
+            break;
+        case '\t':
+            column += 4;
+            break;
+        case '\r':
+        case '\n':
+            line++;
+            column = 1;
+            break;
+        default:
+            return false;
     }
+    return true;
 }
 
-/**
- * Main function.
- * @return 0
- */
-int main() {
+bool isDigit(char ch)
+{
+    return ch >= '0' && ch <= '9';
+}
 
-    string inputFileName;
+bool isLetter(char ch)
+{
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+}
 
-    cout << "Enter the program file name: ";
-    cin >> inputFileName;
-    cout << endl;
+int Reserve(string strToken)
+{
+    int i;
+    for(i = 1; i <= 13; i++)
+    {
+        if(strToken == key[i])
+            return i;
+    }
+    return 0;
+}
 
-	source.open(inputFileName, ios::in); // Read file
-	output.open("results", ios::out | ios::trunc); // Write file
+void Concat(string &str, char ch)
+{
+    str.push_back(ch);
+}
+
+void Retract(fstream &source)
+{
+    if(!source.eof())
+        source.seekg(-1, ios::cur);
+}
+
+
+int main()
+{
+    string infileName;
+    cout << "请输入程序文件名：";
+    cin >> infileName;
+
+    fstream source;
+    fstream output;
+    source.open(infileName, ios::in);
+    output.open("results", ios::out | ios::trunc);
+
     line = 1;
     column = 1;
 
-	if (!source.is_open()) {
-		cout << "Cannot open the source file!\a" << endl;
-		exit(1);
-	}
-	if (!output.is_open()) {
-		cout << "Cannot open the output file!\a" << endl;
-	} else {
-        // Header of the file (DateTime & File name & Lang set)
-
-        time_t rawtime;
-        struct tm * timeinfo;
-
-        time (&rawtime);
-        timeinfo = localtime (&rawtime);
-
-        output << "Generate Time: " << asctime(timeinfo);
-        output << "Program File Name: " << inputFileName << endl;
-        output << "Language Set: PL/0" << endl;
-        output << endl;
+    if(!source.is_open())
+    {
+        cout << "无法打开源文件！" << endl;
+        exit(1);
+    }
+    if(!output.is_open())
+    {
+        cout << "无法打开输出文件！" << endl;
+        exit(0);
     }
 
-	string strToken;
-	char ch;
-	while (!source.eof())
-	{
-        ch = source.get();
+    time_t rawtime;
+    struct tm *timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
 
-        if (isBC(ch)){ // Blank character check
+    output << "生成时间：" << asctime(timeinfo);
+    output << "源文件：" << infileName << endl << endl;
+
+    string strToken;
+    char ch;
+    while(!source.eof())
+    {
+        source.get(ch);
+        if(isBlank(ch))
+        {
             strToken = "";
-        } else if (isLetter(ch)){ // Letter check
-
-			while (isLetter(ch) || isDigit(ch)){ // ID check loop
-				strToken = Concat(strToken, ch);
+        }
+        else if(isLetter(ch))
+        {
+            while(isLetter(ch) || isDigit(ch))
+            {
+                Concat(strToken, ch);
                 column++;
-				ch = source.get();
-			}
+                source.get(ch);
+            }
 
+            Retract(source);
 
-            if (Reserve(strToken)){ // Reserved word check
-//                cout << strToken << ", RESERVED" << endl;
+            if(Reserve(strToken))
+            {
                 output << strToken << ", RESERVED" << endl;
             }
-			else{
-//                cout << strToken << ", ID" << endl;
+            else
+            {
                 output << strToken << ", ID" << endl;
             }
+
             strToken = "";
-
-            Retract();
-
-        } else if (isDigit(ch)){ // Digit check
-			while (isDigit(ch)) {
-				strToken = Concat(strToken, ch);
+        }
+        else if(isDigit(ch))
+        {
+            while (isDigit(ch))
+            {
+                Concat(strToken, ch);
                 column++;
-				ch = source.get();
-			}
-
-            if (isLetter(ch)) {
-                cout << "[Program ERROR]" << " [" << line << "," << column <<"] " << "Invalid ID: " ;
-                output << "[Program ERROR]" << " [" << line << "," << column <<"] " << "Invalid ID: " ;
-
-                while (isLetter(ch) || isDigit(ch)){
-                    strToken = Concat(strToken, ch);
-                    column++;
-                    ch = source.get();
-                }
-
-                cout << "\"" << strToken << "\"" << endl;
-                output << "\"" << strToken << "\"" << endl;
-
-            } else {
-//                cout << strToken << ", INT" << endl;
-                output << strToken << ", INT" << endl;
+                source.get(ch);
             }
 
-            Retract();
+            if(isLetter(ch))
+            {
+                cout << "[Program ERROR][" << line << ", " << column << "] " << "Invalid ID: ";
+                output << "[Program ERROR][" << line << ", " << column << "] " << "Invalid ID: ";
+                while(isLetter(ch) || isDigit(ch))
+                {
+                    Concat(strToken, ch);
+                    column++;
+                    source.get(ch);
+                }
+                cout << strToken << endl;
+                output << strToken << endl;
+            }
+            else
+            {
+                output << strToken << ", INT" << endl;
+            }
+            Retract(source);
             strToken = "";
+        }
+        else if(ch == '=')
+        {
+            column++;
+            output << ch << ", COP" << endl;
+        }
+        else if(ch == '<')
+        {
+            column++;
+            source.get(ch);
+            if(ch == '>')
+            {
+                column++;
+                output << "<>, COP" << endl;
+            }
+            else if(ch == '=')
+            {
+                column++;
+                output << "<=, COP" << endl;
+            }
+            else
+            {
+                output << "<, COP" << endl;
+                Retract(source);
+            }
+        }
+        else if(ch == '>')
+        {
+            column++;
+            source.get(ch);
+            if(ch == '=')
+            {
+                column++;
+                output << ">=, COP" << endl;
+            }
+            else
+            {
+                output << ">, COP" << endl;
+                Retract(source);
+            }
+        }
+        else if(ch == ':')
+        {
+            column++;
+            source.get(ch);
+            if(ch == '=')
+            {
+                column++;
+                output << ":=, AOP" << endl;
+            }
+            else
+            {
+                cout << "[Program ERROR]" << " [" << line << "," << column <<"] " << "Missing '=' near the ':' ;" << endl;
+                output << "[Program ERROR]" << " [" << line << "," << column <<"] " << "Missing '=' near the ':' ;" << endl;
+                Retract(source);
+            }
+        }
+        else if(ch == '+')
+        {
+            column++;
+            output << ch << ", OOP" << endl;
+        }
+        else if(ch == '-')
+        {
+            column++;
+            output << ch << ", OOP" << endl;
+        }
+        else if(ch == '*')
+        {
+            column++;
+            output << ch << ", OOP" << endl;
+        }
+        else if(ch == '/')
+        {
+            column++;
+            output << ch << ", OOP" << endl;
+        }
+        else if(ch == ';')
+        {
+            column++;
+            output << ch << ", EOP" << endl;
+        }
+        else if(ch == '(')
+        {
+            column++;
+            output << ch << ", SOP" << endl;
+        }
+        else if(ch == ')')
+        {
+            column++;
+            output << ch << ", SOP" << endl;
+        }
+        else if(ch == ',')
+        {
+            column++;
+            output << ch << ", SOP" << endl;
+        }
+        else
+        {
+            column++;
+            output << ch << ", UNKNOWN" << endl;
+        }
 
-		} else {
-			switch(ch) { // Other characters check
-				case '=':
-                    column++;
-//					cout << ch << ", COP" << endl;
-                    output << ch << ", COP" << endl;
-					break;
-				case '<':
-                    column++;
-					ch = source.get();
-					if (ch == '>') {
-                        column++;
-//						cout << "<>, COP" << endl;
-                        output << "<>, COP" << endl;
-					} else if (ch == '='){
-                        column++;
-//						cout << "<=, COP" << endl;
-                        output << "<=, COP" << endl;
-					} else {
-//						cout << "<, COP" << endl;
-                        output << "<, COP" << endl;
-						Retract();
-					}
-					break;
-				case '>':
-                    column++;
-					ch = source.get();
-					if (ch == '=') {
-                        column++;
-//						cout << ">=, COP" << endl;
-                        output << ">=, COP" << endl;
-					} else {
-//						cout << ">, COP" << endl;
-						output << ">, COP" << endl;
-						Retract();
-					}
-					break;
-				case ':':
-                    column++;
-					ch = source.get();
-					if (ch == '=') {
-                        column++;
-//						cout << ":=, AOP" << endl;
-						output << ":=, AOP" << endl;
-					} else {
-						cout << "[Program ERROR]" << " [" << line << "," << column <<"] " << "Missing \"=\" near the \":\" ;" << endl;
-						output << "[Program ERROR]" << " [" << line << "," << column <<"] " << "Missing \"=\" near the \":\" ;" << endl;
-						Retract();
-					}
-					break;
-				case '+':
-				case '-':
-				case '*':
-				case '/':
-                    column++;
-//					cout << ch << ", OOP" << endl;
-					output << ch << ", OOP" << endl;
-					break;
-				case ';':
-                    column++;
-//					cout << ch << ", EOP" << endl;
-					output << ch << ", EOP" << endl;
-					break;
-                case '(':
-                case ')':
-                case ',':
-                    column++;
-//                    cout << ch << ", SOP" << endl;
-                    output << ch << ", SOP" << endl;
-                    break;
-				default:
-                    column++;
-//					cout << ch << ", UNKNOWN" << endl;
-                    output << ch << ", UNKNOWN" << endl;
-			}
-		}
-	}
+    }
 
-	source.close();
-	output.close();
+    source.close();
+    output.close();
     cout << endl;
-    cout << "Save results successfully" << endl;
-	return 0;
+    cout << "词法分析成功完成，结果已存入results文件中。" << endl;
+
+    return 0;
 }
--- Lexical_Analysis/LA.h
+++ Lexical_Analysis/LA.h
@@ -1,3 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+using namespace std;
+
+/**
+ * 保留字表
+ */
+const string key[14] = {"", "program", "const", "var", "procedure", "begin",
+                        "if", "then", "while", "call", "read", "write", "end", "do"};
+int line;
+int column;
+
+/**
+ * 判断字符是否为空白字符
+ * @param ch
+ * @return true为真
+ */
+bool isBlank(char ch);
+
+/**
+ * 判断字符是否为数字
+ * @param ch
+ * @return true为真
+ */
+bool isDigit(char ch);
+
+/**
+ * 判断字符是否为字母
+ * @param ch
+ * @return true为真
+ */
+bool isLetter(char ch);
+
+/**
+ * 判断strToken是关键字还是用户自定义的标识符
+ * @param strToken
+ * @return 若为0，则表示strToken
+ */
+int Reserve(string strToken);
+
+/**
+ * 将搜索指示器回调一个字符位置
+ */
+void Retract(fstream &source);
+
+/**
+ * 将ch的内容送入strToken
+ * @param str
+ * @param ch
+ * @return
+ */
+void Concat(string &str, char ch);
